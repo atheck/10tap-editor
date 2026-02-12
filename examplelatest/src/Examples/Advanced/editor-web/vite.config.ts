0b6911be609470cbfd5d3fd5699b7484bc10c9e2
Henrik Fuchs <henrik.fuchs@kraftwerk.io>
2026-02-12 15:15:42 +0100

    chore: update deps

--- examplelatest/src/Examples/Advanced/editor-web/vite.config.ts
+++ examplelatest/src/Examples/Advanced/editor-web/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
+import { resolve } from 'path';
 
 // This config is used to build the web editor into a single file
+
+const libWebPath = resolve(__dirname, '../../../../../lib-web/index.mjs');
 
 export default defineConfig({
   root: 'src/Examples/Advanced/editor-web/',
@@ -12,14 +15,21 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: '@10play/tentap-editor',
-        replacement: '@10play/tentap-editor/web',
+        find: '@10play/tentap-editor/web',
+        replacement: libWebPath,
       },
-      // We alias tiptap view and state to use the internal version of tiptap to avoid this error https://github.com/ueberdosis/tiptap/issues/3869#issuecomment-2167931620      {
-      { find: '@tiptap/pm/view', replacement: '@10play/tentap-editor/web' },
+      {
+        find: '@10play/tentap-editor',
+        replacement: libWebPath,
+      },
+      // We alias tiptap view and state to use the internal version of tiptap to avoid this error https://github.com/ueberdosis/tiptap/issues/3869#issuecomment-2167931620
+      {
+        find: '@tiptap/pm/view',
+        replacement: libWebPath,
+      },
       {
         find: '@tiptap/pm/state',
-        replacement: '@10play/tentap-editor/web',
+        replacement: libWebPath,
       },
     ],
   },
