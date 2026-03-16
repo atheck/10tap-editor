5688a7b4d3962bba1bc8f96359db3ce165343c0e
Henrik Fuchs <henrik.fuchs@kraftwerk.io>
2026-03-16 16:10:32 +0100

    fix: use trailing node extension as is

--- src/bridges/trailingNode.ts
+++ src/bridges/trailingNode.ts
@@ -1,35 +1,6 @@
-import { Extension } from '@tiptap/core';
-import { Plugin, PluginKey } from '@tiptap/pm/state';
+import { TrailingNode } from '@tiptap/extensions';
 import BridgeExtension from './base';
 
-const trailingNodeTypes = ['image', 'table'];
-
-const TrailingNodeExtension = Extension.create({
-  name: 'trailingNode',
-
-  addProseMirrorPlugins() {
-    return [
-      new Plugin({
-        key: new PluginKey('trailingNode'),
-        appendTransaction: (_, __, newState) => {
-          const { doc, tr, schema } = newState;
-          const lastNode = doc.lastChild;
-
-          if (
-            !lastNode ||
-            !trailingNodeTypes.includes(lastNode.type.name) ||
-            !schema.nodes.paragraph
-          ) {
-            return null;
-          }
-
-          return tr.insert(doc.content.size, schema.nodes.paragraph.create());
-        },
-      }),
-    ];
-  },
-});
-
 export const TrailingNodeBridge = new BridgeExtension({
-  tiptapExtension: TrailingNodeExtension,
+  tiptapExtension: TrailingNode,
 });
